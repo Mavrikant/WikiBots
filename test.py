10cf0363e12455b0cc0832d9cfea96a313e9a37e
Mavrikant <m.serdar.karaman@ya.ru>
2015-12-24 01:50:56 +0000

    delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import json
-
-with open(".pass") as json_file:
-    json_data = json.load(json_file)
-
-passw = json_data['Mavrikant'].decode('base64').decode('base64').decode('base64').decode('UTF-8')
-print passw
-print type(passw)
