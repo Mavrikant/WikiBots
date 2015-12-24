1bf3fac3fff7880bf523fd4bce4b9446655956ed
Mavrikant <m.serdar.karaman@ya.ru>
2015-12-24 03:03:19 +0200

    test

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-__author__ = 'mavri'
+# -*- coding: utf-8 -*-
+
+import json
+
+
+with open('.pass') as data_file:
+    data = json.load(data_file)
+
+aaa= data['Mavrikant'].decode('base64').decode('base64').decode('base64').decode('UTF-8')
+print aaa
+print type(aaa)
