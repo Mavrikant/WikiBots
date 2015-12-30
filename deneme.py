ee057153c0d5cf1cb7ab58bb835824dd3532e24f
Mavrikant <m.serdar.karaman@ya.ru>
2015-12-30 23:09:32 +0200

    cronrab için birkaç düzenleme

--- deneme.py
+++ deneme.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
+# !/usr/bin/python
 
 import mavri
 
-xx = mavri.login('tr.wikipedia', 'Mavrikant Bot')
+xx = mavri.login(u'tr.wikipedia', u'Mavrikant Bot')
 
 print xx.text
-print mavri.appendtext_on_page('tr.wikipedia', 'Vikipedi:Deneme tahtası', 'test', 'test', xx).text
+print mavri.appendtext_on_page(u'tr.wikipedia', u'Vikipedi:Deneme tahtası', u'test', u'test', xx).text
