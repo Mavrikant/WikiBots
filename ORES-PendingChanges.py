c3806f63042e16b08632c5469e11cfe1a28e72de
Mavrikant <m.serdar.karaman@ya.ru>
2016-01-03 07:16:12 +0200

    hepsini tara

--- ORES-PendingChanges.py
+++ ORES-PendingChanges.py
@@ -2,39 +2,41 @@
 # !/usr/bin/python
 from bs4 import BeautifulSoup
 import requests
-
 import mavri
 
 xx = mavri.login('tr.wikipedia', 'Mavrikant')
-content = requests.get(
-    'https://tr.wikipedia.org/w/index.php?title=%C3%96zel:BekleyenDe%C4%9Fi%C5%9Fiklikler&dir=prev&limit=500',
-    cookies=xx.cookies).text
-soup = BeautifulSoup(content, 'html.parser')
-
 RAPOR = '{| class="wikitable sortable"\n|-\n! Sayfa !! Damaging !! Reverted'
+trwiki = 'https://tr.wikipedia.org'
+nextpage = '/w/index.php?title=%C3%96zel:BekleyenDe%C4%9Fi%C5%9Fiklikler&limit=100'
+while nextpage != 'DONE':
+    soup = BeautifulSoup(requests.get(trwiki + nextpage, cookies=xx.cookies).text, 'html.parser')
+    try:
+        nextpage = soup.findAll("a", {"class": "mw-nextlink"})[0].get('href')
+    except:
+        nextpage = 'DONE'
 
-for line in soup.find("div", {"id": "mw-content-text"}).ul.find_all('li'):
-    incele = line.find_all('a')[2].get('href')
-    title = line.find_all('a')[0].get('title')
-    incele_text = requests.get('https://tr.wikipedia.org' + incele, cookies=xx.cookies).text
+    for line in soup.find("div", {"id": "mw-content-text"}).ul.find_all('li'):
+        incele = line.find_all('a')[2].get('href')
+        title = line.find_all('a')[0].get('title')
+        incele_text = requests.get(trwiki + incele, cookies=xx.cookies).text
 
-    if incele_text.find('diff-multi') == -1:
-        diff = incele_text.split('<input id="mw-fr-input-oldid" type="hidden" value="')[1].split('"')[0]
-        print title
-        # print diff
-        damaging = \
-            requests.get('http://ores.wmflabs.org/scores/trwiki/damaging/' + str(diff)).json()[str(diff)][
-                'probability'][
-                'true'] * 100
-        reverted = \
-            requests.get('http://ores.wmflabs.org/scores/trwiki/reverted/' + str(diff)).json()[str(diff)][
-                'probability'][
-                'true'] * 100
-        # print damaging
-        # print reverted
-        RAPOR += '\n|-\n| [https://tr.wikipedia.org' + incele + ' ' + title + '] || ' + str(damaging).replace('.',
-                                                                                                              ',') + ' || ' + str(
-            reverted).replace('.', ',')
+        if incele_text.find('diff-multi') == -1:
+            diff = incele_text.split('<input id="mw-fr-input-oldid" type="hidden" value="')[1].split('"')[0]
+            print title
+            # print diff
+            damaging = \
+                requests.get('http://ores.wmflabs.org/scores/trwiki/damaging/' + str(diff)).json()[str(diff)][
+                    'probability'][
+                    'true'] * 100
+            reverted = \
+                requests.get('http://ores.wmflabs.org/scores/trwiki/reverted/' + str(diff)).json()[str(diff)][
+                    'probability'][
+                    'true'] * 100
+            # print damaging
+            # print reverted
+            RAPOR += '\n|-\n| [https://tr.wikipedia.org' + incele + ' ' + title + '] || ' + str(damaging).replace('.',
+                                                                                                                  ',') + ' || ' + str(
+                reverted).replace('.', ',')
 
 RAPOR += '\n|}'
 
