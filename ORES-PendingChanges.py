01a84abcd5d4382e775adfeb8847e070825e4b8c
Mavrikant <m.serdar.karaman@ya.ru>
2016-01-03 06:31:59 +0200

    dzn

--- ORES-PendingChanges.py
+++ ORES-PendingChanges.py
@@ -7,11 +7,11 @@ import mavri
 
 xx = mavri.login('tr.wikipedia', 'Mavrikant')
 content = requests.get(
-    'https://tr.wikipedia.org/w/index.php?title=%C3%96zel:BekleyenDe%C4%9Fi%C5%9Fiklikler&dir=prev&limit=100',
+    'https://tr.wikipedia.org/w/index.php?title=%C3%96zel:BekleyenDe%C4%9Fi%C5%9Fiklikler&dir=prev&limit=500',
     cookies=xx.cookies).text
 soup = BeautifulSoup(content, 'html.parser')
 
-RAPOR = '{| class="wikitable"\n|-\n! Sayfa !! Damaging !! Reverted'
+RAPOR = '{| class="wikitable sortable"\n|-\n! Sayfa !! Damaging !! Reverted'
 
 for line in soup.find("div", {"id": "mw-content-text"}).ul.find_all('li'):
     incele = line.find_all('a')[2].get('href')
@@ -32,10 +32,11 @@ for line in soup.find("div", {"id": "mw-content-text"}).ul.find_all('li'):
                 'true'] * 100
         # print damaging
         # print reverted
-        RAPOR += '\n|-\n| [https://tr.wikipedia.org' + incele + ' ' + title + '] || ' + str(damaging) + ' || ' + str(
-            reverted)
+        RAPOR += '\n|-\n| [https://tr.wikipedia.org' + incele + ' ' + title + '] || ' + str(damaging).replace('.',
+                                                                                                              ',') + ' || ' + str(
+            reverted).replace('.', ',')
 
 RAPOR += '\n|}'
 
-print mavri.change_page('tr.wikipedia', 'Kullanıcı:Mavrikant/ORES', RAPOR, 'ORES', xx)
+print mavri.change_page('tr.wikipedia', 'Kullanıcı:Mavrikant/ORES', RAPOR, 'ORES', xx).text
 exit(0)
