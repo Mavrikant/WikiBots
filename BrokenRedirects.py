cd1b4c54092036bfe24be1e4b39964c2608162bd
M. Serdar Karaman <Mavrikant@users.noreply.github.com>
2016-01-10 15:05:53 +0200

    if redirect:

--- BrokenRedirects.py
+++ BrokenRedirects.py
@@ -13,13 +13,15 @@ for line in soup.find("div", {"id": "mw-content-text"}).ol.find_all('li'):
     page = line.find_all('a')[0].get('title')
     pagetext = requests.get('https://tr.wikipedia.org/w/index.php?title=' + page + '&action=raw').text
     sil = re.findall('\{\{\s?[Ss]il\s?\|?\s?[^\}]*\s?\}\}', pagetext)
-    redirect = re.findall('\[\[\s?([^\]]*)\s?\]\]', pagetext)[0]
-    redirectpagetext = requests.get('https://tr.wikipedia.org/w/index.php?title=' + redirect + '&action=raw').text
+    redirect = re.findall('\[\[\s?([^\]]*)\s?\]\]', pagetext)
+    if redirect:
+        redirectpagetext = requests.get('https://tr.wikipedia.org/w/index.php?title=' + redirect + '&action=raw').text
 
-    if pagetext != '' and redirectpagetext == '' and not sil:
-        HS = '{{Sil | Y1. Var olmayan sayfalara olan yönlendirmeler silinebilir. --~~~~}}\n\n'
-        mavri.change_page('tr.wikipedia', page, HS + pagetext, '+ Hızlı sil, var olmayan sayfaya yönlendirme', xx)
-        mavri.appendtext_on_page('tr.wikipedia', 'Kullanıcı:Mavrikant/Log/BrokenRedirects',
-                                 '\n* [[' + page + ']] -> [[' + redirect + ']]',
-                                 '[[' + page + ']] -> [[' + redirect + ']]', xx)
+        if pagetext != '' and redirectpagetext == '' and not sil:
+            HS = '{{Sil | Y1. Var olmayan sayfalara olan yönlendirmeler silinebilir. --~~~~}}\n\n'
+            mavri.change_page('tr.wikipedia', page, HS + pagetext, '+ Hızlı sil, var olmayan sayfaya yönlendirme', xx)
+            mavri.appendtext_on_page('tr.wikipedia', 'Kullanıcı:Mavrikant/Log/BrokenRedirects',
+                                     '\n* [[' + page + ']] -> [[' + redirect + ']]',
+                                     '[[' + page + ']] -> [[' + redirect + ']]', xx)
+
 exit(0)
