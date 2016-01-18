1f8b103074e0e9f56d3b19b6befe6d3c676305e2
Mavrikant <m.serdar.karaman@ya.ru>
2016-01-18 19:20:01 +0200

    dzn

--- VP-GununMaddesi.py
+++ VP-GununMaddesi.py
@@ -5,42 +5,42 @@ import mavri
 import datetime
 from random import randint
 
-wiki = 'tr.wikiquote'
+wiki = 'tr.wikipedia'
 xx = mavri.login(wiki, 'Mavrikant Bot')
 
 one_day = datetime.timedelta(days=1)
 now = datetime.datetime.now()
-ilk = datetime.date(2015, 1, 1)
+ilk = datetime.date(2015, 9, 1)
 bugun = datetime.date(now.year, now.month, now.day)
-yarin = bugun + one_day
+yarin = bugun + one_day + one_day
 kaynak = ilk + randint(0, int(str(bugun - ilk).split(' days')[0])) * one_day
 
-Log_page = 'Kullanıcı:Mavrikant Bot/Log/Günün Sözü'
+Log_page = 'Kullanıcı:Mavrikant Bot/Log/Günün Maddesi'
 
 
 if (yarin.strftime("%d") == '01'):  # Yeni ay temizliği
     mavri.page_clear(wiki, Log_page, 'Yeni ay temizliği', xx)
 
-YARIN = mavri.content_of_page(wiki, 'Vikisöz:Günün sözü/' + yarin.strftime("%Y/%m/%d"))
+YARIN = mavri.content_of_page(wiki, 'Şablon:GM/' + yarin.strftime("%Y-%m-%d"))
 
-if (YARIN == ''): # yarın boş
+if (YARIN == ''):  # yarın boş
     Summary = 'Olumsuz'
     Durum = '\n* {{Çapraz}}'
 
     # Kaynak söz bul ve al
     KAYNAK = ''
     while KAYNAK == '':
-        KAYNAK = mavri.content_of_page(wiki, 'Vikisöz:Günün sözü/' + kaynak.strftime("%Y/%m/%d"))
+        KAYNAK = mavri.content_of_page(wiki, 'Şablon:GM/' + kaynak.strftime("%Y-%m-%d"))
         kaynak += one_day
 
     # Kaynak söz ile yarını oluştur
-    mavri.change_page(wiki, 'Vikisöz:Günün sözü/' + yarin.strftime("%Y/%m/%d"), KAYNAK,
-                       '[[Vikisöz:Günün sözü/' + kaynak.strftime("%Y/%m/%d") + ']] sayfasından kopyalandı.', xx)
+    mavri.change_page(wiki, 'Şablon:GM/' + yarin.strftime("%Y-%m-%d"), KAYNAK,
+                       '[[Şablon:GM/' + kaynak.strftime("%Y-%m-%d") + ']] sayfasından kopyalandı.', xx)
 
 else:  # yarın dolu
     Summary = 'Olumlu'
     Durum = '\n* {{Tamam}}'
 
-Durum += ' [[Vikisöz:Günün sözü/' + yarin.strftime("%Y/%m/%d") + ' | \'\'\'' + yarin.strftime("%d.%m.%Y") + '\'\'\']]'
+Durum += ' [[Şablon:GM/' + yarin.strftime("%Y-%m-%d") + ' | \'\'\'' + yarin.strftime("%d.%m.%Y") + '\'\'\']]'
 
 mavri.sent_message(wiki, Log_page, Durum, Summary, xx)
