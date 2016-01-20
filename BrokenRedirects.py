58ed5f67da6830b392c6b48632ec50aec12b26d6
Mavrikant <m.serdar.karaman@ya.ru>
2016-01-20 02:17:27 +0200

    güncelleme

--- BrokenRedirects.py
+++ BrokenRedirects.py
@@ -1,27 +1,36 @@
 # -*- coding: utf-8 -*-
 # !/usr/bin/python
-from bs4 import BeautifulSoup
-import requests
-import mavri
 import re
 
-xx = mavri.login('tr.wikipedia', 'Mavrikant')
+import requests
+from bs4 import BeautifulSoup
+
+import mavri
+
+wiki = 'tr.wikipedia'
+xx = mavri.login(wiki, 'Mavrikant')
 content = requests.get('https://tr.wikipedia.org/wiki/%C3%96zel:BozukY%C3%B6nlendirmeler').text
 soup = BeautifulSoup(content, 'html.parser')
 
 for line in soup.find("div", {"id": "mw-content-text"}).ol.find_all('li'):
     page = line.find_all('a')[0].get('title')
-    pagetext = requests.get('https://tr.wikipedia.org/w/index.php?title=' + page + '&action=raw').text
-    sil = re.findall('\{\{\s?[Ss]il\s?\|?\s?[^\}]*\s?\}\}', pagetext)
-    redirect = re.findall('\[\[\s?([^\]]*)\s?\]\]', pagetext)
-    if redirect:
-        redirectpagetext = requests.get('https://tr.wikipedia.org/w/index.php?title=' + redirect + '&action=raw').text
-
-        if pagetext != '' and redirectpagetext == '' and not sil:
-            HS = '{{Sil | Y1. Var olmayan sayfalara olan yönlendirmeler silinebilir. --~~~~}}\n\n'
-            mavri.change_page('tr.wikipedia', page, HS + pagetext, '+ Hızlı sil, var olmayan sayfaya yönlendirme', xx)
-            mavri.appendtext_on_page('tr.wikipedia', 'Kullanıcı:Mavrikant/Log/BrokenRedirects',
-                                     '\n* [[' + page + ']] -> [[' + redirect + ']]',
-                                     '[[' + page + ']] -> [[' + redirect + ']]', xx)
+    print page
+    pagetext = mavri.content_of_page(wiki, page)
+    print pagetext
+    if pagetext != '':
+        sil = re.findall('\{\{\s?[Ss]il[^\}]*\}\}', pagetext)
+        print sil
+        redirect = re.findall('\[\[\s?([^\]]*)\s?\]\]', pagetext)
+        print redirect
+        if redirect:
+            redirectpagetext = mavri.content_of_page(wiki, redirect)
+            print redirectpagetext
+            if redirectpagetext == '' and not sil:
+                HS = '{{Sil | Y1. Var olmayan sayfalara olan yönlendirmeler silinebilir. --~~~~}}\n\n'
+                mavri.change_page('tr.wikipedia', page, HS + pagetext, '+ Hızlı sil, var olmayan sayfaya yönlendirme',
+                                  xx)
+                mavri.appendtext_on_page('tr.wikipedia', 'Kullanıcı:Mavrikant/Log/BrokenRedirects',
+                                         '\n* [[' + page + ']] -> [[' + redirect + ']]',
+                                         '[[' + page + ']] -> [[' + redirect + ']]', xx)
 
 exit(0)
