6e8fcc5854fc567aedffab34e6c9f1a658c9d90f
M. Serdar Karaman <Mavrikant@users.noreply.github.com>
2017-04-27 05:08:28 +0300

    Update TemplateReplace.py

--- TemplateReplace.py
+++ TemplateReplace.py
@@ -10,7 +10,7 @@ import json
 
 def read_param(parameter):
     if parameter in parameters:
-        result = parameters[parameter].capitalize()
+        result = parameters[parameter].capitalize().rstrip()
         if result is ('Sm' or 'Sl' or 'Km'):
             result = result.upper()
         return result
@@ -34,30 +34,33 @@ while ticontinue != 'DONE':
         ticontinue = 'DONE'
 
     for page in allpages.json()['query']['pages'].itervalues().next()['transcludedin']:
-        title = page['title']
-        ns = page['ns']
-        print title
-        content = mavri.content_of_section('tr.wikipedia', title, 0, xx)
-        # print content
-        paramet = re.findall(ur'\{\{\s*[Vv]ikiProje [Ss]por([^\}]*)\}\}', content)[0].split("|")
-        parameters = {}
-        for para in paramet:
-            if "=" in para:
-                parameters[para.split("=")[0]] = para.split("=")[1]
+        try:
+            title = page['title']
+            ns = page['ns']
+            print title
+            content = mavri.content_of_section('tr.wikipedia', title, 0, xx)
+            # print content
+            paramet = re.findall(ur'\{\{\s*[Vv]ikiProje [Ss]por([^\}]*)\}\}', content)[0].split("|")
+            parameters = {}
+            for para in paramet:
+                if "=" in para:
+                    parameters[para.split("=")[0]] = para.split("=")[1]
 
-        replacement = "{{VikiProje |Proje = " + VikiProje_name + " |Sınıf = " + read_param(
-            u'sınıf') + " |Önem = " + read_param(u'önem') + " }}"
+            replacement = "{{VikiProje |Proje = " + VikiProje_name + " |Sınıf = " + read_param(
+                u'sınıf') + " |Önem = " + read_param(u'önem') + " }}"
 
-        content = re.sub(ur'\{\{\s*[Vv]ikiProje [Ss]por([^\}]*)\}\}', replacement, content)
-        content = re.sub(ur'\{\{\s*[Tt]artışma\s*\}\}\n?', '', content)
-        content = '{{Tartışma}}\n' + content
-        if ns is 1:
-            entity = mavri.wikibase_item(wiki, title.split(':')[1])
-            if (mavri.wbgetclaims(entity, 'P570').text == '{"claims":{}}') and (
-                        '"id":"Q5"' in mavri.wbgetclaims(entity, 'P31').text):
-                content = re.sub(ur'\{\{\s*[Yy][İi][Bb]\s*\}\}\n?', '', content)
-                content = '{{YİB}}\n' + content
+            content = re.sub(ur'\{\{\s*[Vv]ikiProje [Ss]por([^\}]*)\}\}', replacement, content)
+            content = re.sub(ur'\{\{\s*[Tt]artışma\s*\}\}\n?', '', content)
+            content = '{{Tartışma}}\n' + content
+            if ns is 1:
+                entity = mavri.wikibase_item(wiki, title.split(':')[1])
+                if (mavri.wbgetclaims(entity, 'P570').text == '{"claims":{}}') and (
+                            '"id":"Q5"' in mavri.wbgetclaims(entity, 'P31').text):
+                    content = re.sub(ur'\{\{\s*[Yy][İi][Bb]\s*\}\}\n?', '', content)
+                    content = '{{YİB}}\n' + content
 
-        # print "======="
-        # print content
-        mavri.change_section(wiki, title, 0, content, summary, xx)
+            # print "======="
+            # print content
+            mavri.change_section(wiki, title, 0, content, summary, xx)
+        except:
+            print "error"
