e054bbdfcd17fc5e4fc73cae4fa1a03b3ae57f45
M. Serdar Karaman <Mavrikant@users.noreply.github.com>
2017-05-21 18:54:59 +0300

    ilk çalışan version

--- bekleyen-vproje.py
+++ bekleyen-vproje.py
@@ -5,8 +5,55 @@ from bs4 import BeautifulSoup
 import re
 import mavri
 
-xx = mavri.login('tr.wikipedia', 'Mavrikant Bot')
 wiki='tr.wikipedia'
+xx = mavri.login(wiki, 'Mavrikant Bot')
+
+results = {
+u"10K": [],
+u"Antarktika": [],
+u"Azerbaycan": [],
+u"Balkanlar": [],
+u"Bilim": [],
+u"Buffy evreni": [],
+u"Din": [],
+u"Edebiyat": [],
+u"Eurovision": [],
+u"Fransa": [],
+u"Futbol": [],
+u"GM": [],
+u"Hande Yener": [],
+u"Harry Potter": [],
+u"Hristiyanlık": [],
+u"Hukuk": [],
+u"Kafkasya": [],
+u"Kimya": [],
+u"Kürt sineması": [],
+u"Kıbrıs": [],
+u"LGBT": [],
+u"Lady Gaga": [],
+u"MHB": [],
+u"Madonna": [],
+u"Müzik": [],
+u"Orta Doğu": [],
+u"Osmanlı": [],
+u"Otomobil": [],
+u"Rihanna": [],
+u"Sanat": [],
+u"Sinema": [],
+u"Siyaset": [],
+u"Sovyetler Birliği": [],
+u"Spor": [],
+u"Tarih": [],
+u"Tenis": [],
+u"Tiyatro": [],
+u"Türkiye": [],
+u"Tıp": [],
+u"Veteriner Tıp": [],
+u"Çin": [],
+u"İnternet": [],
+u"İstanbul": [],
+u"İsviçre": []
+}
 
 nextpage = '.org/w/index.php?title=Özel:BekleyenDeğişiklikler&dir=prev&limit=50'
 while nextpage != 'DONE':
@@ -18,10 +65,22 @@ while nextpage != 'DONE':
 
     for line in soup.find("div", {"id": "mw-content-text"}).ul.find_all('li'):
         title = line.find_all('a')[0].get('title')
+        incele = line.find_all('a')[2].get('href')
         talk_page="Talk:"+title
 
-        content=mavri.content_of_section(wiki, talk_page, 0, xx)
+        content = mavri.content_of_section(wiki, talk_page, 0, xx)
         projects = re.findall(ur'\{\{\s*[Vv]ikiProje\s*\|\s*[Pp]roje\s*\=\s*([^\|]*)', content)
+        print title
         print projects
+        for project in projects:
+            results[project.strip()].append(title)
+    #nextpage = 'DONE'
+    #print results
 
+    for project in results:
+        content='Güncellenme tarihi: {{subst:CURRENTDAY}} {{subst:CURRENTMONTHNAME}} {{subst:CURRENTYEAR}} {{subst:CURRENTDAYNAME}} {{subst:CURRENTTIME}}'
+        for title in results[project]:
+            content=content+'\n#: [['+title+']] ([https://tr.wikipedia.org/w/index.php?title='+title+'&action=history geçmiş])'
+
+        mavri.change_page(wiki, 'User:Mavrikant Bot/Bekleyen/Proje/'+project, content, 'Güncelleme', xx)
 exit(0)
